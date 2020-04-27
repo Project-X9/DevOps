c0f229f1ce2dce3ef5c2109128dd3e201bf0c142
omar <omar3bdellatif@gmail.com>
2020-04-27 13:56:45 +0200

    Final Phase 2nd commit

---
+++ Test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_accountoverview.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_artist.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_changePassword.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_likedsongs.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_loggedOutHome.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_login.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_playlist.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_premium.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_signup.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_webplayerHome.py -m Do
+
+python3 -m pytest --alluredir="./Reports/All_Reports/allurefiles" ./Web_Testing/Tests/test_yourLibrary.py -m Do
+
+
