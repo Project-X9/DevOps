38347fa7395172665384cf381ccbb62a15a7f94a
omar3bdellatif <39925465+omar3bdellatif@users.noreply.github.com>
2020-05-14 06:23:16 +0200

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#PLEASE READ THE LAST NOTE IN THE IMPORTANTGUIDELINES.TXT FILE
-#NOTE THAT IF YOU WANT TO RUN THIS SCRIPT ON A REMOTE SERVER
-#PLEASE REPLACE every "omar" with the "ubuntu"
+cd /home/omar/Downloads
+
+sudo git clone https://github.com/Project-X9/BackEnd.git
 
 cd /home/omar/Downloads
 
-sudo git clone -b devops https://github.com/Project-X9/BackEnd.git
-
-cd /home/omar/Downloads
-
-sudo git clone -b Deployed https://github.com/Project-X9/FrontEnd.git
+sudo git clone https://github.com/Project-X9/FrontEnd.git
 
 cd /home/omar/Downloads
 sudo chown -R omar BackEnd
