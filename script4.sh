27e17c35950e2e080469f9cf607681e2a9271d7b
omar3bdellatif <39925465+omar3bdellatif@users.noreply.github.com>
2020-05-14 06:23:55 +0200

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-#PLEASE READ THE LAST NOTE IN THE IMPORTANTGUIDELINES.TXT FILE
-#NOTE THAT IF YOU WANT TO RUN THIS SCRIPT ON A REMOTE SERVER
-#PLEASE REPLACE every "omar" with the "ubuntu"
-
 cd /home/omar/Downloads
 
 git clone -b DevOpsB https://github.com/Project-X9/Testing.git
 
 cd Testing
 sudo apt install python3-pip
-sudo apt install python-pip
 sudo apt-get install python3-distutils
 
-sudo apt install curl
 
-curl https://bootstrap.pypa.io/get-pip.py | python
 python3 -m pip install -r requirements.txt
 sudo apt-get install xvfb
-
 #READY TO RUN TESTS
 
 python3 -m pytest ./Web_Testing/Tests/test_changePassword.py -m Do
